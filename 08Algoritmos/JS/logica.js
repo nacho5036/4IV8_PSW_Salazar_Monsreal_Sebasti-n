1079aedaa931476dfc7a1be2ec48e5ce4d3f751c
nacho5036 <ssalazarm2000@alumno.ipn.mx>
2022-03-16 10:45:01 -0600

    Algoritmias

---
+++ 08Algoritmos/JS/logica.js
@@ -0,0 +1,53 @@
+//problema 3
+
+function problema3(){
+
+    //definir un alfabeto
+    var alfabeto = ['A', 'B', 'C', 'D', 'E', 
+        'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+        'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T',
+        'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    //vamos a obtener la entrada de los datos
+    var p3_input = document.querySelector('#p3-input').value;
+
+    var p3_palabras = p3_input.split(',');
+
+    //tengo que eliminar los espacios
+
+    p3_palabras = p3_palabras.map(function (palabra){
+        //crear una expresion que me recupere las palabras
+        return palabra.replace(/\s/g, '').toUpperCase();
+    });
+
+    //tengo que calcular cuantos caracteres unicos hay
+    var p3_res = '';
+    
+    p3_palabras.forEach(function (palabra, i){
+        //separar las palabras actuales del array que 
+        //contiene cada letra de dicha palabra
+        var letras_unicas = [];
+        var palabra_array = palabra.split('');
+
+        //iteracion
+        alfabeto.forEach(function (letra, J){
+            //itero la palabra
+            palabra_array.forEach(function (letras_palabras, k){
+
+                //comprobar que la letra este dentro del alfabeto
+                if(letras_palabras == letra){
+                    //si la letra no la hemos contado
+                    //la agregamos a un array para contar
+                    //las letras unicas
+                    if(letras_unicas.indexOf(letra)<0){
+                        letras_unicas.push(letra);
+                    }
+                }
+            });
+        });
+        p3_res += 'Palabra: ' + palabra + ' = ' + letras_unicas.length + '\n';
+    });
+    document.querySelector('#p3-output').textContent = p3_res;
+   
+
+}
