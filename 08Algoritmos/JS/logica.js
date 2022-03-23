de3841cfe1547fbbc3925ecd93ddb5822d4eae1a
nacho5036 <ssalazarm2000@alumno.ipn.mx>
2022-03-22 23:33:04 -0600

    Algoritmia

--- 08Algoritmos/JS/logica.js
+++ 08Algoritmos/JS/logica.js
@@ -1,4 +1,36 @@
-//problema 3
+//Problema 1 
+function problema1(){ 
+    var p1_input = document.querySelector('#p1-input').value;
+    var p1_array = p1_input.split(' ').reverse(); 
+    var p1_res = ''; 
+    
+    p1_array.forEach(function (palabra, i){
+        if(i != 0 || i != p1_array.length) p1_res += ' '; 
+        p1_res += palabra;
+    });
+
+    document.querySelector('#p1-output').textContent = p1_res; 
+}
+
+//Problema 2 
+function problema2(){
+    var x1 = parseInt(document.getElementById('p2-x1').value);
+    var x2 = parseInt(document.getElementById('p2-x2').value);
+    var x3 = parseInt(document.getElementById('p2-x3').value);
+    var x4 = parseInt(document.getElementById('p2-x4').value);
+    var x5 = parseInt(document.getElementById('p2-x5').value);
+
+    var y1 = parseInt(document.getElementById('p2-y1').value);
+    var y2 = parseInt(document.getElementById('p2-y2').value);
+    var y3 = parseInt(document.getElementById('p2-y3').value);
+    var y4 = parseInt(document.getElementById('p2-y4').value);
+    var y5 = parseInt(document.getElementById('p2-y5').value);
+
+    document.getElementById('p2-output').innerHTML = (x1*y1)+(x2*y2)+(x3*y3)+(x4*y4)+(x5*y5)
+
+}
+
+//problema 3 
 
 function problema3(){
 
